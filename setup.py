f1819da0bde7bacb70390262dcd347d36081a701
bxz9200 <bingyiz@clemson.edu>
2025-01-27 15:10:06 +0800

    update setup.py for pip installation

--- setup.py
+++ setup.py
@@ -9,6 +9,9 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     url='https://github.com/betterdataai/time-series-synthetic.git',
     license='MIT',
-    install_requires=['einops', 'numpy', 'matplotlib', 'scikit-learn', 'wandb', 'pandas', 'supervised-fcn-2', 'numba', 'x-transformers==1.31.6', 'torch==2.2.1', 'pytorch-lightning==2.3.1', 'seaborn']
+    install_requires=['imbalanced-learn==0.12.3', 'imblearn==0.0', 'jsonschema==4.23.0', 'pycountry==22.3.5', 'scipy==1.14.0', 'transformers==4.40.2', 'tqdm==4.66.5',
+                      'joblib==1.4.2', 'pdoc3==0.11.1', 'docstring_parser==0.16', 'psutil==5.9.0', 'rstr==3.2.2', 'pytorch_tabnet==4.1.0',
+                      'einops', 'numpy==1.26.4', 'matplotlib', 'scikit-learn==1.4.2', 'wandb', 'pandas==2.2.2', 'supervised-fcn-2', 'numba', 'x-transformers==1.31.6',
+                      'torch==2.4.0', 'pytorch-lightning==2.3.1', 'seaborn']
 
 )
